2885548a8aa69ca8bafe8654464fdcf955e44312
divyansh-bhatt <divyansh.bhatt_cs22@gla.ac.in>
2026-02-18 21:56:59 +0530

    [Divyansh Bhatt]add.added UC1 question

---
+++ feature_UC1-FeetEquality/src/test/java/com/QuantityMeasurementApp/AppTest.java
@@ -0,0 +1,49 @@
+package com.QuantityMeasurementApp;
+import junit.framework.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+class QuantityMeasurementAppTest {
+
+    @org.junit.Test
+    void testEquality_SameValue() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(1.0);
+
+        assertTrue(f1.equals(f2), "1.0 ft should be equal to 1.0 ft");
+    }
+
+    @org.junit.Test
+    void testEquality_DifferentValue() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(2.0);
+
+        assertFalse(f1.equals(f2), "1.0 ft should not be equal to 2.0 ft");
+    }
+
+    @org.junit.Test
+    void testEquality_NullComparison() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+
+        assertFalse(f1.equals(null), "Value should not be equal to null");
+    }
+
+    @org.junit.Test
+    void testEquality_SameReference() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+
+        assertTrue(f1.equals(f1), "Object should be equal to itself");
+    }
+
+    @org.junit.Test
+    void testEquality_NonNumericInput() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+
+        assertFalse(f1.equals("Not a Feet Object"), "Feet object should not be equal to a non-Feet object");
+    }
+}
