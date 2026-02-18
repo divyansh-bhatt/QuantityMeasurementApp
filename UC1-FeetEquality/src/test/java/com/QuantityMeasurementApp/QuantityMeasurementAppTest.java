45e09a2ccf9e4cd2b36b64c95ebeb235d10f03ef
divyansh-bhatt <divyansh.bhatt_cs22@gla.ac.in>
2026-02-18 22:08:31 +0530

    [Divyansh Bhatt]add.added UC1 question

--- UC1-FeetEquality/src/test/java/com/QuantityMeasurementApp/QuantityMeasurementAppTest.java
+++ UC1-FeetEquality/src/test/java/com/QuantityMeasurementApp/QuantityMeasurementAppTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-class QuantityMeasurementAppTest {
+public class QuantityMeasurementAppTest {
 
     @org.junit.Test
     void testEquality_SameValue() {
