c5a65d7e73a88cb3f612501cac7775da26690b42
divyansh-bhatt <divyansh.bhatt_cs22@gla.ac.in>
2026-03-09 23:34:42 +0530

    [Divyansh Bhatt]add.added UC8-standaloneunit

--- src/main/java/org/example/LengthUnit.java
+++ src/main/java/org/example/LengthUnit.java
@@ -5,23 +5,23 @@ public enum LengthUnit {
     FEET(1.0),
     INCHES(1.0 / 12.0),
     YARDS(3.0),
-    CENTIMETERS(0.393701 / 12.0);
+    CENTIMETERS(1.0 / 30.48);
 
-    private final double factorToFeet;
+    private final double conversionFactorToFeet;
 
-    LengthUnit(double factorToFeet) {
-        this.factorToFeet = factorToFeet;
+    LengthUnit(double factor) {
+        this.conversionFactorToFeet = factor;
     }
 
-    public double toFeet(double value) {
-        return value * factorToFeet;
+    public double convertToBaseUnit(double value) {
+        return value * conversionFactorToFeet;
     }
 
-    public double fromFeet(double feetValue) {
-        return feetValue / factorToFeet;
+    public double convertFromBaseUnit(double baseValue) {
+        return baseValue / conversionFactorToFeet;
     }
 
-    public double getFactor() {
-        return factorToFeet;
+    public double getConversionFactor() {
+        return conversionFactorToFeet;
     }
 }
