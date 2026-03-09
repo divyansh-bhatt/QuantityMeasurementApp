86d04b997d8720636bc03a1306a5b5396c7e2963
divyansh-bhatt <divyansh.bhatt_cs22@gla.ac.in>
2026-03-09 23:22:51 +0530

    [Divyansh Bhatt]add.added UC5-unitconversion

---
+++ quantitymeasurementapp/src/test/java/QuantityMeasurementTest.java
@@ -0,0 +1,71 @@
+import java.org.example.*;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class QuantityMeasurementTest {
+
+    private static final double EPSILON = 1e-6;
+
+    @Test
+    void testConversion_FeetToInches() {
+        assertEquals(12.0,
+                QuantityMeasurementApp.convert(1.0, LengthUnit.FEET, LengthUnit.INCHES),
+                EPSILON);
+    }
+
+    @Test
+    void testConversion_YardsToInches() {
+        assertEquals(36.0,
+                QuantityMeasurementApp.convert(1.0, LengthUnit.YARDS, LengthUnit.INCHES),
+                EPSILON);
+    }
+
+    @Test
+    void testConversion_CentimetersToInches() {
+        assertEquals(1.0,
+                QuantityMeasurementApp.convert(2.54, LengthUnit.CENTIMETERS, LengthUnit.INCHES),
+                EPSILON);
+    }
+
+    @Test
+    void testConversion_RoundTrip() {
+
+        double original = 5.0;
+
+        double converted =
+                QuantityMeasurementApp.convert(original, LengthUnit.FEET, LengthUnit.INCHES);
+
+        double roundTrip =
+                QuantityMeasurementApp.convert(converted, LengthUnit.INCHES, LengthUnit.FEET);
+
+        assertEquals(original, roundTrip, EPSILON);
+    }
+
+    @Test
+    void testConversion_ZeroValue() {
+        assertEquals(0.0,
+                QuantityMeasurementApp.convert(0.0, LengthUnit.FEET, LengthUnit.INCHES),
+                EPSILON);
+    }
+
+    @Test
+    void testConversion_NegativeValue() {
+        assertEquals(-12.0,
+                QuantityMeasurementApp.convert(-1.0, LengthUnit.FEET, LengthUnit.INCHES),
+                EPSILON);
+    }
+
+    @Test
+    void testConversion_InvalidUnit_Throws() {
+        assertThrows(IllegalArgumentException.class,
+                () -> QuantityMeasurementApp.convert(1.0, null, LengthUnit.FEET));
+    }
+
+    @Test
+    void testConversion_NaN_Throws() {
+        assertThrows(IllegalArgumentException.class,
+                () -> QuantityMeasurementApp.convert(Double.NaN, LengthUnit.FEET, LengthUnit.INCHES));
+    }
+}
