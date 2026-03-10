fd60ad8eed83abaa6e43aaa30b4579885e51c26d
divyansh-bhatt <divyansh.bhatt_cs22@gla.ac.in>
2026-03-10 16:09:44 +0530

    [Divyansh Bhatt]add.added UC11-VolumeMeasurement

---
+++ quantitymeasurementapp/src/main/java/org/example/VolumeUnit.java
@@ -0,0 +1,22 @@
+package java.org.example;
+
+public enum VolumeUnit implements IMeasurable {
+
+    LITRE(1.0),
+    MILLILITRE(0.001),
+    GALLON(3.78541);
+
+    private final double factor;
+
+    VolumeUnit(double factor) {
+        this.factor = factor;
+    }
+
+    public double getConversionFactor() {
+        return factor;
+    }
+
+    public String getUnitName() {
+        return name();
+    }
+}
